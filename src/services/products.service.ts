df66c5239f968943ce749113bbabde05965b80a9
Thiago Medeiros <medeiroshs@hotmail.com>
2022-07-29 16:15:34 -0300

    feat: criando o JOI para validar o corpo da requisição na camada de service

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,3 +1,4 @@
+import Joi from 'joi';
 import connection from '../models/connection';
 import ProductsModel from '../models/products.model';
 import Products from '../interfaces/products.interface';
@@ -8,6 +9,19 @@ class ProductsService {
   constructor(model: ProductsModel = new ProductsModel(connection)) {
     this.productModel = model;
   }
+
+  validationBody = (data: object) => {
+    const schema = Joi.object({
+      name: Joi.string().required().min(3),
+      amount: Joi.string().required().min(3),
+    });
+
+    const { error, value } = schema.validate(data);
+
+    if (error) throw error;
+
+    return value;
+  };
 
   async getAll(): Promise<Products[]> {
     const products = await this.productModel.getAll();
