33600445bf0438d01eca028536740022ae3d2e00
Thiago Medeiros <medeiroshs@hotmail.com>
2022-07-29 18:30:18 -0300

    feat: criando a route do auth para fazer o login na rota post

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import AuthController from '../controllers/auth.controller';
+
+const authRouter = Router();
+
+const authController = new AuthController();
+
+authRouter.post('/', authController.login);
+
+export default authRouter;
