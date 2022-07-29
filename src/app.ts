3942be9231b37099845a5f92634324a8562a382c
Thiago Medeiros <medeiroshs@hotmail.com>
2022-07-29 17:17:21 -0300

    feat: colocando a rota /orders no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import errorHandlerMiddleware from './middlewares/error.midleware';
+import ordersRouter from './routes/orders.route';
 import productsRouter from './routes/products.route';
 import usersRouter from './routes/users.route';
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
+app.use('/orders', ordersRouter);
 
 app.use(errorHandlerMiddleware);
   
