cc0935114c07680faaec71af60f7f289648b6352
Thiago Medeiros <medeiroshs@hotmail.com>
2022-07-29 16:14:40 -0300

    feat: criando o midleware de erro junto com express-async-errors

---
+++ src/middlewares/error.midleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+
+const errors: Record<string, number> = {
+  ValidationError: 400,
+};
+
+const errorHandlerMiddleware = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  const status = errors[err.name];
+  if (!status) return res.sendStatus(500);
+  if (err.message.includes('must be')) {
+    res.status(422).json({ message: err.message });
+  }
+  res.status(status).json({ message: err.message });
+};
+
+export default errorHandlerMiddleware;
